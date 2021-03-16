e20b407a38249501f742eec207e3b77557334683
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2021-03-16 20:12:40 +0545

    Configure Templates in settings.py
    In order to use the Django Template Language (DTL), we first need to make some configurations in our Django project.

--- halkapan/settings.py
+++ halkapan/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -51,10 +52,15 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'halkapan.urls'
 
+# Templates Directory
+TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
+
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # Add 'TEMPLATE_DIR' here
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
