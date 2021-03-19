7ce383dec05f9bc54835b9bfb7405eaa96348190
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2021-03-19 17:31:05 +0545

    Add Login Registration Feature
    Creating custom form & and custom html for login and register

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+# External Import
+from django.shortcuts import render, redirect
+from django.contrib.auth import views as auth_views
 
-# Create your views here.
+# Internal Import
+from . import forms
+
+
+class LoginView(auth_views.LoginView):
+    form_class = forms.LoginForm
+    template_name = 'accounts/login.html'
+
+
+def register(request):
+
+    form = forms.UserRegistrationForm(request.POST or None)
+    if form.is_valid():
+        user = form.save(commit=False)
+        user.active = False
+        user.save()
+        print("hello")
+        return redirect("accounts:login")
+
+    return render(request, 'accounts/register.html', {'form': form})
