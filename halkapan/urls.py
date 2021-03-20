158d527be5796cec83e14ea0e461e9b802bcc507
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2021-03-20 10:06:39 +0545

    Create New App posts
    Added app to installed apps in setting. managed the urls for the app posts

--- halkapan/urls.py
+++ halkapan/urls.py
@@ -8,6 +8,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('', include('posts.urls')),
 ]
 
 if settings.DEBUG:
---
+++ posts/urls.py
@@ -0,0 +1,12 @@
+# External Import
+from django.urls import path
+
+# Internal Import
+from . import views
+
+# App Name
+app_name = 'posts'
+
+urlpatterns = [
+
+]
