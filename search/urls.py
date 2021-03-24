65f5a04df7aeb51373734dd47d528e67263b794a
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2021-03-24 18:13:42 +0545

    Implement Search Feature
    Now the search works on title only later on I will implement on another field also

--- search/urls.py
+++ search/urls.py
@@ -2,10 +2,11 @@
 from django.urls import path
 
 # Internal Import
+from . import views
 
 app_name = 'search'
 
 urlpatterns = [
-
+    path('', views.SearchPostListView.as_view(), name='query')
 ]
 
--- search/views.py
+++ search/views.py
@@ -1,3 +1,24 @@
+# External Import
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+# Internal Import
+from posts.models import Post
+
+
+class SearchPostListView(ListView):
+    template_name = "search/search-view.html"
+    paginate_by = 20
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        query = self.request.GET.get('q')
+        context["query"] = query
+        return context
+
+    def get_queryset(self):
+        request = self.request
+        query = request.GET.get('q', None)
+        if query is not None and query != '':
+            return Post.objects.search(query)
+        return Post.objects.none()
