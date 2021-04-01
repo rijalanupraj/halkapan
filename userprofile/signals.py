c7395fd8d3179425bbc7763697cea6d442fee8e2
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2021-04-01 19:44:33 +0545

    Add signal for User profile

--- userprofile/signals.py
+++ userprofile/signals.py
@@ -1,7 +1,8 @@
 # External Import
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.conf import settings
 from django.dispatch import receiver
+import os
 
 # Internal Import
 from .models import Profile
@@ -18,3 +19,19 @@ def create_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+
+@receiver(pre_save, sender=Profile)
+def delete_old_file(sender, instance, **kwargs):
+    if instance._state.adding and not instance.pk:
+        return False
+
+    try:
+        old_image = sender.objects.get(pk=instance.pk).image
+    except sender.DoesNotExist:
+        return False
+
+    if os.path.isfile(old_image.path):
+        if not "default-profile-picture.jpg" in old_image.path:
+            os.remove(old_image.path)
+            print("removed")
