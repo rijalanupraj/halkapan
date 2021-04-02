4f3fd263ccbb11dd142ac28f914a0c07d104d229
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2021-04-02 19:25:32 +0545

    Add Tag List View

--- tags/views.py
+++ tags/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+# External Import
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 
-# Create your views here.
+# Internal Import
+from .models import Tag
+from posts.models import Post
+
+
+class PostTagListView(ListView):
+    template_name = "tags/tag-list-view.html"
+
+    def get_queryset(self):
+        tag = get_object_or_404(Tag, slug=self.kwargs['slug'])
+        return Post.objects.all().filter(tag=tag)
