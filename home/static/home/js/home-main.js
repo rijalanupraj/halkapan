205874e548c61afa6d3148afa0d0b2e1db9ff81f
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2021-04-08 21:47:58 +0545

    Add frontend of home page
    Still having error with counter up

---
+++ home/static/home/js/home-main.js
@@ -0,0 +1,7 @@
+// jQuery counterUp
+$(document).ready(function ($) {
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 1000
+    });
+});
--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,13 @@
 # External Import
 from django.urls import path
-
+from django.shortcuts import render
 app_name = 'home'
 
-urlpatterns = [
 
+def index(request):
+    return render(request, "home/home.html")
+
+
+urlpatterns = [
+    path('', index)
 ]
