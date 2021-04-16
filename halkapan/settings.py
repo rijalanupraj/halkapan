610c6c54ba880cfb0743ebef10ad98e4fe96eeea
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2021-04-16 09:23:47 +0545

    Set up django message framework

--- halkapan/settings.py
+++ halkapan/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -167,9 +168,19 @@ CKEDITOR_CONFIGS = {
     },
 }
 
+# Email Verification
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = "halkapan123@gmail.com"
 EMAIL_HOST_PASSWORD = "Hello@123"
 EMAIL_PORT = 587
+
+# Message
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
---
+++ static/js/main.js
@@ -0,0 +1,8 @@
+// For Messages Alert
+function myAlertBottom() {
+    $(".myAlert-bottom").show();
+    setTimeout(function () {
+        $(".myAlert-bottom").hide();
+    }, 5000);
+}
+myAlertBottom();
