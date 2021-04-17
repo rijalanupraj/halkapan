2e0d2432be53f246dd018aea7fb4e17f8f451085
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2021-04-18 00:39:37 +0545

    Admin panel added
    Still there are many errors

--- myadmin/urls.py
+++ myadmin/urls.py
@@ -1,8 +1,17 @@
 # External Import
 from django.urls import path
 
+# Internal Import
+from . import views
+
 app_name = 'myadmin'
 
 urlpatterns = [
+    path('', views.dashboard, name='admin-dashboard'),
+    path('users/', views.UserListView.as_view(), name='admin-users'),
+    path('delete-user/<str:username>', views.delete_user, name='delete-user'),
+    path('make-staff/<str:username>', views.make_staff, name="make-staff"),
+    path('make-normal-user/<str:username>',
+         views.make_normal_user, name="make-normal-user"),
 
 ]
