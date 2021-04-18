eca3c96938c884c02380bafbb38437aee7551373
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2021-04-18 12:18:25 +0545

    Reset verification email done

--- accounts/views.py
+++ accounts/views.py
@@ -27,16 +27,33 @@ class LoginView(auth_views.LoginView):
     template_name = 'accounts/login.html'
 
 
-# def register(request):
+def resend_verification(request):
+    form = forms.SendEmailVerificationForm(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            user = User.objects.filter(email=email)
+            if user:
+                user = user.first()
+                current_site = get_current_site(request)
+                email_subject = 'Activate Your Account'
+                message = render_to_string('accounts/activate_account.html', {
+                    'user': user,
+                    'domain': current_site.domain,
+                    'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                    'token': account_activation_token.make_token(user),
+                })
+                to_email = form.cleaned_data.get('email')
+                email = EmailMessage(email_subject, message, to=[to_email])
+                email.send()
+                messages.success(
+                    request, f'We have sent you an email, please confirm your email address to complete registration')
+                return redirect('login')
+            else:
+                messages.error(
+                    request, f'The Account with that Email doesn\'t Exist')
+    return render(request, 'accounts/resend_verification.html', {'form': form})
 
-#     form = forms.UserRegistrationForm(request.POST or None)
-#     if form.is_valid():
-#         user = form.save(commit=False)
-#         user.is_active = False
-#         user.save()
-#         return redirect("login")
-
-#     return render(request, 'accounts/register.html', {'form': form})
 
 def register(request):
     form = forms.UserRegistrationForm(request.POST or None)
