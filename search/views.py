a42c61e01ec5e09040000c8bf266978d61e5a303
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2021-04-18 16:25:15 +0545

    Dynamic Loading

--- search/views.py
+++ search/views.py
@@ -8,7 +8,7 @@ from posts.models import Post
 
 class SearchPostListView(ListView):
     template_name = "search/search-view.html"
-    paginate_by = 20
+    paginate_by = 1
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
